205a4d70a958d8d2fda44fb2a10b4657b54628a6
ShaliniMoses <41835638+ShaliniMoses@users.noreply.github.com>
2018-07-28 23:50:59 +0530

    TShirtOrder-Assignment

---
+++ TShirtOrder/src/test/java/test/steps/Methoddefinition.java
@@ -0,0 +1,132 @@
+package test.steps;
+
+import org.apache.struts.action.Action;
+import org.mortbay.log.Log;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import com.gargoylesoftware.htmlunit.ElementNotFoundException;
+
+public class Methoddefinition {
+	public WebDriver driver;
+	public Methoddefinition() {
+	}
+	public Methoddefinition(WebDriver driver)
+	{
+		this.driver = driver;
+	}
+	public boolean validatewebsite()
+	{
+		WebElement logoelement=driver.findElement(By.xpath("//img[contains(@src,'logo.jpg')]"));
+		boolean status=logoelement.isDisplayed();
+		System.out.println("Status"+status);
+		if(status=true){
+			Log.info("Website Exist");
+		}else{
+			Log.info("No Such Website");
+		}
+		return status;
+	}
+	
+	public boolean ClickBtn(WebElement element) throws ElementNotFoundException
+	{
+		boolean status=element.isDisplayed();
+		System.out.println("Status"+status);
+		if(status=true){
+			Log.info("Element Exist");
+			element.click();
+		}else{
+			Log.info("No Such Element");
+		}
+		return status;
+	}
+	public boolean enterText(WebElement element, String value) throws ElementNotFoundException
+	{
+		boolean status=element.isDisplayed();
+		System.out.println("Status"+status);
+		if(status=true){
+			Log.info("Element Exist");
+			element.sendKeys(value);
+		}else{
+			Log.info("No Such Element");
+		}
+		
+		return status;
+	}
+	
+	public void validatePage(WebElement element) throws ElementNotFoundException
+	{
+		boolean status=element.isDisplayed();
+		System.out.println("Status"+status);
+		if(status=true){
+			Log.info("Page Validation Successful");
+		}else{
+			Log.info("Page Validation Failed");
+		}
+		
+	}
+	
+	public void tshirtoptions(String size, String color) throws ElementNotFoundException
+	{
+		WebElement smallsize=driver.findElement(By.xpath("//input[@id='layered_id_attribute_group_1']"));
+		WebElement mediumsize=driver.findElement(By.xpath("//input[@id='layered_id_attribute_group_2']"));
+		WebElement largesize=driver.findElement(By.xpath("//input[@id='layered_id_attribute_group_3']"));
+		
+		WebElement orangecolor=driver.findElement(By.xpath("//input[@id='layered_id_attribute_group_13']"));
+		WebElement bluecolor=driver.findElement(By.xpath("//input[@id='layered_id_attribute_group_14']"));
+		
+		if(size.equalsIgnoreCase("small")){
+			if(smallsize.isSelected()){
+				Log.info("Already Selected");
+			}else{
+				smallsize.click();
+			}
+			
+		}if(size.equalsIgnoreCase("medium")){
+			if(mediumsize.isSelected()){
+				Log.info("Already Selected");
+			}else{
+				mediumsize.click();
+			}
+		}if(size.equalsIgnoreCase("large")){
+			if(largesize.isSelected()){
+				Log.info("Already Selected");
+			}else{
+				largesize.click();
+			}
+		}
+		if(color.equalsIgnoreCase("Orange")){
+			if(orangecolor.isSelected()){
+				Log.info("Already Selected");
+			}else{
+				orangecolor.click();
+			}
+			
+		}if(color.equalsIgnoreCase("Blue")){
+			if(bluecolor.isSelected()){
+				Log.info("Already Selected");
+			}else{
+				bluecolor.click();
+			}
+		}
+	}
+		public void orderhistory() throws ElementNotFoundException
+		{
+		WebElement instock=driver.findElement(By.xpath("//div[@class='product-container']//following::*[@class='availability']"));
+		WebElement addtocart=driver.findElement(By.xpath("//*[text()='Add to cart']"));
+		Actions act=new Actions(driver);
+		act.moveToElement(instock).perform();
+		addtocart.click();
+		
+		WebElement successmsg=driver.findElement(By.xpath("//div[@id='layer_cart']//following::*[@class='icon-ok']"));
+		driver.switchTo().activeElement();
+		if(successmsg.isDisplayed()){
+			Log.info("T-Shirt added to cart");
+		}else{
+			Log.info("Functionality failed");
+		}
+		
+	}
+}
---
+++ TShirtOrder/src/test/java/test/steps/ProductOrderTest.java
@@ -0,0 +1,105 @@
+package test.steps;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.mortbay.log.Log;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class ProductOrderTest {
+
+	public WebDriver driver;
+	Methoddefinition methoddef = new Methoddefinition(driver);
+	Properties prop = new Properties();
+	
+	@Given("^I open the browser$")
+	public void Open_Browser() throws Throwable {
+//			DesiredCapabilities capability = DesiredCapabilities.chrome();
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\Downloads\\chromedriver_win32\\chromedriver");
+			driver = new ChromeDriver();
+			driver.get("http://automationpractice.com/index.php");
+			driver.manage().timeouts().implicitlyWait(500, TimeUnit.SECONDS);
+	}
+	@And("^automation website exist$")
+	public void automation_website_exist() throws Throwable {
+		
+		boolean elementpresent=methoddef.validatewebsite();
+		System.out.println(elementpresent);
+		if(elementpresent=true){
+			Log.info("Website Exist");
+		}else{
+			Log.info("No Such Website");
+			driver.close();
+		}
+	}
+
+	@Then("^I click on signin button$")
+	public void i_click_on_signin_button() throws Throwable {
+		WebElement signin=driver.findElement(By.xpath("//a[@class='login']"));
+		methoddef.ClickBtn(signin);
+	}
+
+	@When("^I enter the username \"([^\"]*)\"$")
+	public void i_enter_the_username(String UserName) throws Throwable {
+		WebElement uname=driver.findElement(By.xpath("//*[@id='email']"));
+		methoddef.enterText(uname,UserName);
+	}
+
+	@When("^I enter the password  \"([^\"]*)\"$")
+	public void i_enter_the_password(String password) throws Throwable {
+		WebElement pwd=driver.findElement(By.xpath("//*[@id='passwd']"));
+		methoddef.enterText(pwd,password);
+	}
+
+	@When("^Click on Login button$")
+	public void click_on_Login_button() throws Throwable {
+		WebElement login=driver.findElement(By.xpath("//*[contains(text(),'Sign in')]"));
+		methoddef.ClickBtn(login);
+	}
+
+	@Then("^I validate the logged in page$")
+	public void i_validate_the_logged_in_page() throws Throwable {
+		WebElement signoutelement=driver.findElement(By.xpath("//*[contains(text(),'Sign out')]"));
+		methoddef.validatePage(signoutelement);
+		
+	}
+
+	@Then("^I click T-Shirts$")
+	public void i_click_T_Shirts() throws Throwable {
+		WebElement tshirtoption=driver.findElement(By.xpath("//a[text()='T-shirts']"));
+		methoddef.ClickBtn(tshirtoption);
+		
+	}
+	@Then("^I select T-Shirts properties \"([^\"]*)\",\"([^\"]*)\"$")
+	public void i_select_T_Shirts_properties(String size, String color) throws Throwable {
+		WebElement tshirtoption=driver.findElement(By.xpath("//a[text()='T-shirts']"));
+		methoddef.ClickBtn(tshirtoption);
+		methoddef.tshirtoptions(size, color);
+	}
+	
+	@Then("^I Validate the order History$")
+	public void i_Validate_the_order_History() throws Throwable {
+		methoddef.orderhistory();
+	}
+	@Then("^I click on signout button$")
+	public void i_click_on_signout_button() throws Throwable {
+		WebElement signoutelement=driver.findElement(By.xpath("//*[contains(text(),'Sign out')]"));
+		methoddef.ClickBtn(signoutelement);
+		
+	}
+	@Then("^I close the browser$")
+	public void i_close_the_browser() throws Throwable {
+		driver.close();
+	}
+	
+}
+
+
